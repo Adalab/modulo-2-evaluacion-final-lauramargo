4ed03e41f08b56f0365cd02b515ddbfdc29b5b08
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-07-15 19:03:14 +0200

    create render and show api

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,33 @@
 'use strict';
 
-/*console.log('>> Ready :)');*/
 
+//globales
+const textUser = document.querySelector('.js-textUser');
+const btnSearch = document.querySelector('.js-btnSearch');
+const btnReset = document.querySelector('.js-btnReset');
+const ul = document.querySelector('.js-ul');
+
+let data = [];
+
+// pintar datos de la API
+const renderSeries = () => {
+    let html = '';
+    for (const series of seriesAnime) {
+        html += `<li><article><img src='${series.images.jpg.image_url}' />`;
+        html += `<h2>${series.title}</h2>`;
+        html += `</article></li>`;
+    }
+    ul.innerHTML = html;
+};
+//pedir datos del servidor
+const showApi = () => {
+    fetch(' https://api.jikan.moe/v4/anime')
+
+        .then((response) => response.json())
+        .then((dataSeries) => {
+            seriesAnime = dataSeries.data;
+            renderSeries();
+        });
+};
+
+showApi();
